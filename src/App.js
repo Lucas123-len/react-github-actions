407830f3f3811228c18c944f7cafe7081d21f8dc
Lucas123-len <69940678+Lucas123-len@users.noreply.github.com>
2022-10-06 22:10:23 -0300

    Atualização do App.js através de um fork.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Raphael Collin
+          Lucas Pereira Rangel
         </p>
         <a
           className="App-link"
